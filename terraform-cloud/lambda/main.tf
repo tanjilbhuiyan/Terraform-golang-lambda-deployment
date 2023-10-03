1665c1147c2049ea3c827e515fa2c262b16760d3
tanjilbs23 <tanjil.bs23@gmail.com>
2023-10-03 13:11:37 +0600

    local exec command change

--- terraform-cloud/lambda/main.tf
+++ terraform-cloud/lambda/main.tf
@@ -2,7 +2,7 @@
 resource "terraform_data" "install_golang" {
   # Run the build script
   provisioner "local-exec" {
-    command = "wget https://go.dev/dl/go1.21.1.linux-amd64.tar.gz && tar -xzf go1.21.1.linux-amd64.tar.gz && export GOPATH=$HOME/go && export PATH=$PATH:$GOPATH/bin && go version"
+    command = "wget https://go.dev/dl/go1.21.1.linux-amd64.tar.gz && tar -xzf go1.21.1.linux-amd64.tar.gz && ls -la && pwd && export GOPATH=$HOME/go && export PATH=$PATH:$GOPATH/bin && go version"
   }
   provisioner "local-exec" {
     command = "cd src && GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ../bin/handler && cd ../bin && zip handler.zip handler"
