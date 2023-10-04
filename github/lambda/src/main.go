2411c6bda13cfdc097a379c739bf08b32a89734a
tanjilbs23 <tanjil.bs23@gmail.com>
2023-10-05 02:03:07 +0600

    file change test

--- github/lambda/src/main.go
+++ github/lambda/src/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func handler(ctx context.Context) (string, error) {
-	message := "Hello, World! file change test success"
+	message := "Hello, World! file change test success 2nd test"
 	fmt.Println(message)
 	return message, nil
 }
