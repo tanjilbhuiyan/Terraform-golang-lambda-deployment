3d51274afe02915e12af1826b2a9076f4155e88c
tanjilbs23 <tanjil.bs23@gmail.com>
2023-10-05 01:50:11 +0600

    adding lifecycle rule for the bucket newer noncurrent version

--- github/lambda/main.tf
+++ github/lambda/main.tf
@@ -53,7 +53,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "builds" {
     status = "Enabled"
 
     noncurrent_version_expiration {
-      noncurrent_days           = 10 # Expire noncurrent versions after 90 days
+      noncurrent_days           = 1 # Expire noncurrent versions after 90 days
       newer_noncurrent_versions = 2
     }
 
