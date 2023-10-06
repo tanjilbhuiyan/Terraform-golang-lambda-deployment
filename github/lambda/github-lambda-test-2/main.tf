58fb7dcd20b289a6edcecc4ca6622f2f73ff724e
tanjilbs23 <tanjil.bs23@gmail.com>
2023-10-06 18:54:12 +0600

    testing if lambda uploads if there are no changes

--- github/lambda/github-lambda-test-2/main.tf
+++ github/lambda/github-lambda-test-2/main.tf
@@ -1,34 +1,3 @@
-# # Zip the build file
-# data "archive_file" "lambda_go_zip" {
-#   type        = "zip"
-#   source_file = "${path.module}/bin/bootstrap"
-#   output_path = "${path.module}/bin/handler.zip"
-# }
-
-# Build and upload package to S3
-
-
-# # Download package from S3 
-
-# # Lambda module to deploy using terraform
-# module "lambda_function" {
-#   source = "terraform-aws-modules/lambda/aws"
-
-#   function_name = "tf-cloud-golang-lambda-using-github"
-#   description   = "This lambda is being deployed from github"
-#   handler       = "bootstrap"
-#   runtime       = "provided.al2"
-#   architectures = ["arm64"]
-
-#   create_package         = false
-#   local_existing_package = "${path.module}/bin/handler.zip"
-
-
-# }
-
-
-
-
 # Download package from S3 
 data "aws_s3_object" "application_zip" {
   bucket = var.s3_bucket
