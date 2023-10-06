9a164573a19350bd043f982e7b70629ae312f249
tanjilbs23 <tanjil.bs23@gmail.com>
2023-10-06 16:14:30 +0600

    testing script

--- github/main.tf
+++ github/main.tf
@@ -1,11 +1,11 @@
-# module "golang-lambda-1" {
-#   source    = "./lambda/github-lambda-test-1"
-#   s3_bucket = module.golang-lambda-bucket.s3-bucket-function_name
-# }
-# module "golang-lambda-2" {
-#   source    = "./lambda/github-lambda-test-2"
-#   s3_bucket = module.golang-lambda-bucket.s3-bucket-function_name
-# }
+module "golang-lambda-1" {
+  source    = "./lambda/github-lambda-test-1"
+  s3_bucket = module.golang-lambda-bucket.s3-bucket-function_name
+}
+module "golang-lambda-2" {
+  source    = "./lambda/github-lambda-test-2"
+  s3_bucket = module.golang-lambda-bucket.s3-bucket-function_name
+}
 
 
 module "golang-lambda-bucket" {
