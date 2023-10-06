de894a4e25dc4a40636bea917c16cad9fd2c0931
tanjilbs23 <tanjil.bs23@gmail.com>
2023-10-06 16:42:24 +0600

    modified pipeline

--- .github/scripts/golang-build-deploy.sh
+++ .github/scripts/golang-build-deploy.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # Set your AWS S3 bucket name
-S3_BUCKET_NAME="my-builds-for-golang-lambda-test"
+S3_BUCKET_NAME="your-s3-bucket-name"
 
 # Read the file and process each line
 while IFS= read -r line; do
   # Remove leading and trailing whitespace and quotes
   line=$(echo "$line" | sed -e 's/^"//' -e 's/"$//')
 
-  # Extract the directory path from the line
-  dir_path=$(dirname "$line")
+  # Remove the "github/lambda/" prefix from the path
+  modified_line=$(echo "$line" | sed -e 's/github\/lambda\///')
+
+  # Extract the directory path from the modified line
+  dir_path=$(dirname "$modified_line")
+
+  # Extract the filename without extension
+  file_name=$(basename "$modified_line" .go)
 
   # Navigate to the directory
   cd "$dir_path" || { echo "Failed to change directory to $dir_path"; exit 1; }
@@ -17,14 +23,14 @@ while IFS= read -r line; do
   # Build the Go code with specified parameters
   GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o bootstrap
 
-  # Create a ZIP file
-  zip -q -r bootstrap.zip bootstrap
+  # Create a ZIP file with the desired filename
+  zip -q -r "$file_name.zip" bootstrap
 
   # Upload the ZIP file to S3
-  aws s3 cp bootstrap.zip "s3://$S3_BUCKET_NAME/$(basename "$line").zip"
+  aws s3 cp "$file_name.zip" "s3://$S3_BUCKET_NAME/$file_name.zip"
 
   # Clean up
-  rm -f bootstrap bootstrap.zip
+  rm -f bootstrap "$file_name.zip"
 
   # Return to the original directory
   cd - || { echo "Failed to return to the original directory"; exit 1; }
