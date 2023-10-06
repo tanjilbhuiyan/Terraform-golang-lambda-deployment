36bfc60cc09b85152ea803d2922ea9a5eafbdda8
tanjilbs23 <tanjil.bs23@gmail.com>
2023-10-06 17:01:59 +0600

    testing pipeline for building golang lambda

--- .github/scripts/golang-build-deploy.sh
+++ .github/scripts/golang-build-deploy.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Read the list.txt file and extract the file paths
+# Read each line from list.txt
 while IFS= read -r line; do
-  path=$(echo "$line" | sed 's#github/##;s#/#/#g;s/src.*//')
-  echo "Processing $path"
-  
-  # Change directory to the extracted path
-  cd "$path"
-  
-  # Run the build command
+  # Remove the leading "github/" and trailing "/src/main.go" from the line
+  file_path=$(echo "$line" | sed 's/github\///;s/\/src\/main.go//')
+
+  # Navigate to the directory
+  cd "$file_path" || { echo "Error: Could not change directory to $file_path"; exit 1; }
+
+  # Run the Go build command
   GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o bootstrap
-  
+
+  # Check if the build was successful
+  if [ $? -eq 0 ]; then
+    echo "Build successful in $file_path"
+  else
+    echo "Error: Build failed in $file_path"
+  fi
+
   # Return to the original directory
   cd -
 done < list.txt
