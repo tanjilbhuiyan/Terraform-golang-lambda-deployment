4673857b1dd3dd15d3dda761582b06fbb2625d5e
tanjilbs23 <tanjil.bs23@gmail.com>
2024-05-16 13:11:20 +0600

    Bucket name changed

--- s3/main.tf
+++ s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "builds" {
-  bucket = "my-unique-bucket-golang-lambda-test-Ran@2021"
+  bucket = "my-unique-bucket-golang-lambda-test"
 }
 
 # Define ownership controls for the bucket
